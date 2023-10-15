c64d8c993bffb0e41396da55d443a89d19baa53e
Marek Długajczyk <marek@mdpdesign.pl>
2023-10-15 11:52:13 +0200

    Make Animator work..

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"math/rand"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
 type FrameConfig struct {
@@ -17,11 +24,94 @@ type FrameConfig struct {
 	Frames []string
 }
 
+func clearText(s string) string {
+	s = strings.ReplaceAll(s, "\"", "")
+	s = strings.ReplaceAll(s, "'", "")
+
+	return s
+}
+
 func randomDelay(maxDelay float32) {
 	r := 0 + rand.Float32()*maxDelay
 	time.Sleep(time.Duration(r * float32(time.Second)))
 }
 
-func main() {
+func clearTerminal() {
+	c := exec.Command("clear")
+	c.Stdout = os.Stdout
+	c.Run()
+}
 
+func main() {
+	fileList := []string{
+		"001-boot.yaml",
+		"002-boot.yaml",
+		"003-load.yaml",
+		"004-welcome.yaml",
+		"005-confirm.yaml",
+		"006-outro.yaml",
+	}
+
+	clearTerminal()
+
+	for _, file := range fileList {
+		fCfg := FrameConfig{}
+
+		data, err := os.ReadFile(file)
+		if err != nil {
+			log.Fatalf("error occurred while reading file: %s, %v", file, err)
+		}
+
+		err = yaml.Unmarshal([]byte(data), &fCfg)
+		if err != nil {
+			log.Fatalf("error parsing Yaml file: %v", err)
+		}
+
+		clear := fCfg.Directives.Clear
+		fullRedraw := fCfg.Directives.FullRedraw
+		loop := fCfg.Directives.Loop
+		splitByLine := fCfg.Directives.SplitByLine
+		maxDelay := fCfg.Directives.MaxDelay
+		endNewLines := fCfg.Directives.EndNewLines
+
+		end := "\n"
+		if clear {
+			end = "\r"
+		}
+
+		if maxDelay == 0 {
+			maxDelay = 0.5
+		}
+
+		for _, frame := range fCfg.Frames {
+			if loop > 0 {
+				frames := fCfg.Frames
+
+				for i := 0; i < loop; i++ {
+					fmt.Printf("%s\r", frames[i%len(frames)])
+					time.Sleep(time.Duration(maxDelay * float32(time.Second)))
+				}
+			} else if splitByLine {
+				frameParts := strings.Split(frame, "\n")
+
+				for _, line := range frameParts {
+					line = clearText(line)
+					fmt.Printf("%s%s", line, end)
+					randomDelay(maxDelay)
+				}
+			} else {
+				if fullRedraw {
+					clearTerminal()
+				}
+
+				fmt.Printf("%s%s", clearText(frame), end)
+				randomDelay(maxDelay)
+			}
+		}
+
+		if endNewLines > 0 {
+			newLinesToPrint := strings.Repeat("\n", endNewLines)
+			fmt.Print(newLinesToPrint)
+		}
+	}
 }
