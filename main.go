b1785a12b88c0ba99cda5cf6797ad4764cc86420
Marek Długajczyk <marek@mdpdesign.pl>
2023-10-16 19:38:48 +0200

    Simplify animation files

--- main.go
+++ main.go
@@ -19,14 +19,16 @@ type YamlConfig struct {
 
 type FrameConfig struct {
 	Directives struct {
-		Clear       bool    `yaml:"clear,omitempty"`
-		FullRedraw  bool    `yaml:"fullRedraw,omitempty"`
-		SplitByLine bool    `yaml:"splitByLine,omitempty"`
+		Type        string  `yaml:"type"`
 		Loop        int     `yaml:"loop,omitempty"`
 		MaxDelay    float32 `yaml:"maxDelay,omitempty"`
 		EndNewLines int     `yaml:"endNewLines,omitempty"`
 	}
 	Frames []string `yaml:"frames"`
+}
+
+func getAnimationtypes() []string {
+	return []string{"clear-line", "clear-screen", "loop", "printer"}
 }
 
 func clearText(s string) string {
@@ -83,45 +85,48 @@ func main() {
 			log.Fatalf("error parsing Yaml file: %v", err)
 		}
 
-		clear := fCfg.Directives.Clear
-		fullRedraw := fCfg.Directives.FullRedraw
+		animationType := fCfg.Directives.Type
 		loop := fCfg.Directives.Loop
-		splitByLine := fCfg.Directives.SplitByLine
 		maxDelay := fCfg.Directives.MaxDelay
 		endNewLines := fCfg.Directives.EndNewLines
-
-		end := "\n"
-		if clear {
-			end = "\r"
-		}
 
 		if maxDelay == 0 {
 			maxDelay = 0.5
 		}
 
 		for _, frame := range fCfg.Frames {
-			if loop > 0 {
+			switch animationType {
+			case "loop":
 				frames := fCfg.Frames
 
 				for i := 0; i < loop; i++ {
 					fmt.Printf("%s\r", frames[i%len(frames)])
 					time.Sleep(time.Duration(maxDelay * float32(time.Second)))
 				}
-			} else if splitByLine {
+			case "printer":
 				frameParts := strings.Split(frame, "\n")
 
 				for _, line := range frameParts {
 					line = clearText(line)
-					fmt.Printf("%s%s", line, end)
+					fmt.Printf("%s\n", line)
 					randomDelay(maxDelay)
 				}
-			} else {
-				if fullRedraw {
-					clearTerminal()
-				}
-
-				fmt.Printf("%s%s", clearText(frame), end)
+			case "clear-line":
+				fmt.Printf("%s\r", clearText(frame))
 				randomDelay(maxDelay)
+			case "clear-screen":
+				clearTerminal()
+
+				fmt.Printf("%s", clearText(frame))
+				randomDelay(maxDelay)
+			default:
+				clearTerminal()
+
+				log.Fatalf(
+					"error animation type '%s' not supported, allowed types: %s",
+					fCfg.Directives.Type,
+					getAnimationtypes(),
+				)
 			}
 		}
 
