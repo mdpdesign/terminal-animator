58b31ccc7b58b2e1f2c03ceb5a635a40646fcd21
mdpdesign <marek.mdproject@gmail.com>
2023-10-22 11:47:26 +0000

    Refactor animations to use interface

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	"os/exec"
-	"strings"
-	"time"
 
 	"gopkg.in/yaml.v3"
 )
@@ -26,28 +21,6 @@ type FrameConfig struct {
 		EndNewLines int     `yaml:"endNewLines,omitempty"`
 	}
 	Frames []string `yaml:"frames"`
-}
-
-func getAnimationtypes() []string {
-	return []string{"clear-line", "clear-screen", "loop", "printer"}
-}
-
-func clearText(s string) string {
-	s = strings.ReplaceAll(s, "\"", "")
-	s = strings.ReplaceAll(s, "'", "")
-
-	return s
-}
-
-func randomDelay(maxDelay float32) {
-	r := 0 + rand.Float32()*maxDelay
-	time.Sleep(time.Duration(r * float32(time.Second)))
-}
-
-func clearTerminal() {
-	c := exec.Command("clear")
-	c.Stdout = os.Stdout
-	c.Run()
 }
 
 func main() {
@@ -86,74 +59,7 @@ func main() {
 			log.Fatalf("error parsing Yaml file: %v", err)
 		}
 
-		animationType := fCfg.Directives.Type
-		loop := fCfg.Directives.Loop
-		typeWriterWordSplit := fCfg.Directives.Word
-		maxDelay := fCfg.Directives.MaxDelay
-		endNewLines := fCfg.Directives.EndNewLines
-
-		if maxDelay == 0 {
-			maxDelay = 0.5
-		}
-
-		for _, frame := range fCfg.Frames {
-			switch animationType {
-			case "typewriter":
-
-				var format string
-				var frameParts []string
-
-				if typeWriterWordSplit {
-					frameParts = strings.Split(frame, " ")
-					format = "%s "
-				} else {
-					frameParts = strings.Split(frame, "")
-					format = "%s"
-				}
-
-				for _, el := range frameParts {
-					fmt.Printf(format, el)
-					randomDelay(maxDelay)
-				}
-
-				fmt.Println("")
-			case "printer":
-				frameParts := strings.Split(frame, "\n")
-
-				for _, line := range frameParts {
-					line = clearText(line)
-					fmt.Printf("%s\n", line)
-					randomDelay(maxDelay)
-				}
-			case "loop":
-				frames := fCfg.Frames
-
-				for i := 0; i < loop; i++ {
-					fmt.Printf("%s\r", frames[i%len(frames)])
-					time.Sleep(time.Duration(maxDelay * float32(time.Second)))
-				}
-			case "clear-line":
-				fmt.Printf("%s\r", clearText(frame))
-				randomDelay(maxDelay)
-			case "clear-screen":
-				clearTerminal()
-
-				fmt.Printf("%s", clearText(frame))
-				randomDelay(maxDelay)
-			default:
-				clearTerminal()
-
-				log.Fatalf(
-					"error animation type '%s' not supported, allowed types: %s",
-					fCfg.Directives.Type,
-					getAnimationtypes(),
-				)
-			}
-		}
-
-		if endNewLines > 0 {
-			newLinesToPrint := strings.Repeat("\n", endNewLines)
-			fmt.Print(newLinesToPrint)
-		}
+		a := MakeAnimation(fCfg)
+		a.Render()
 	}
 }
